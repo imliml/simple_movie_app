d3aa9c72fc485adb97216ec7a06bee988ffaa8fd
lsr <imlimr1232@gmail.com>
2020-08-15 17:10:19 +0900

    loading states and funtion Components

--- src/App.js
+++ src/App.js
@@ -5,42 +5,32 @@ import "./App.css";
 
 class App extends React.Component {
   // 상태값 초기 선언을 해주는 위치
-  state = {
-    greeting: "Hello!",
-    movies: [
-      {
-        title: "Matrix",
-        poster:
-          "https://images-na.ssl-images-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UY1200_CR84,0,630,1200_AL_.jpg",
-      },
-      {
-        title: "Full Metal Jacket",
-        poster:
-          "https://upload.wikimedia.org/wikipedia/en/9/99/Full_Metal_Jacket_poster.jpg",
-      },
-      {
-        title: "Oldboy",
-        poster:
-          "https://upload.wikimedia.org/wikipedia/en/b/bb/Oldboy_2013_film_poster.jpg",
-      },
-      {
-        title: "Star Wars",
-        poster:
-          "https://i.pinimg.com/736x/a1/61/c7/a161c7034917936f8bd53ca65bc6f0d0--star-wars-stuff-art-illustrations.jpg",
-      },
-    ],
-  };
+  state = {};
   // 라이프사이클 선언
   componentDidMount() {
     setTimeout(() => {
       this.setState({
-        greeting: "Hi!",
+        greeting: "Hello!",
         movies: [
-          ...this.state.movies,
           {
-            title: "Trainspotting",
+            title: "Matrix",
             poster:
-              "https://resizing.flixster.com/OUEArjor-MbyCV6GqLU85Hk9jQI=/300x300/v1.bjs1NTQ3OTM7ajsxNzQ2NjsxMjAwOzIwMDA7MTMzMQ",
+              "https://images-na.ssl-images-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UY1200_CR84,0,630,1200_AL_.jpg",
+          },
+          {
+            title: "Full Metal Jacket",
+            poster:
+              "https://upload.wikimedia.org/wikipedia/en/9/99/Full_Metal_Jacket_poster.jpg",
+          },
+          {
+            title: "Oldboy",
+            poster:
+              "https://upload.wikimedia.org/wikipedia/en/b/bb/Oldboy_2013_film_poster.jpg",
+          },
+          {
+            title: "Star Wars",
+            poster:
+              "https://i.pinimg.com/736x/a1/61/c7/a161c7034917936f8bd53ca65bc6f0d0--star-wars-stuff-art-illustrations.jpg",
           },
         ],
       });
@@ -49,16 +39,18 @@ class App extends React.Component {
 
   // 함수선언
 
+  _renderMovies = () => {
+    const movies = this.state.movies.map((movie, index) => {
+      return <Movie title={movie.title} poster={movie.poster} key={index} />;
+    });
+    return movies;
+  };
+
   // return은 화면상 보여지는거
   render() {
     return (
       <div className="App">
-        <h1>{this.state.greeting}</h1>
-        {this.state.movies.map((movie, index) => {
-          return (
-            <Movie title={movie.title} poster={movie.poster} key={index} />
-          );
-        })}
+        {this.state.movies ? this._renderMovies() : "Loading"}
       </div>
     );
   }
